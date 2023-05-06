62b2d296444a389aaf0662972eab82076b5c6d15
naveen p <naveenpendekatla0407@gmail.com>
2023-05-06 15:27:01 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -1,4 +1,18 @@
-resource "aws_instance" "frontend" {
+data "aws_ami" "centos" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+output "ami" {
+  value = data.aws_ami.centos.image_id
+}
+
+
+
+
+
+  resource "aws_instance" "frontend" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
