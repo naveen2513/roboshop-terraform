f0a6bc442586af625b42dd4f6228cdcdefb3b33e
naveen p <naveenpendekatla0407@gmail.com>
2023-05-09 22:31:23 +0530

    terraform

---
+++ data.tf
@@ -0,0 +1,12 @@
+data "aws_ami" "centos" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+
+
+data "aws_security_group" "allow-all" {
+  name = "allow-all"
+}
+
