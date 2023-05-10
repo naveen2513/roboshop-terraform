b95de4b3b32be23863432d6ee3469003dd4847dc
naveen p <naveenpendekatla0407@gmail.com>
2023-05-10 07:39:12 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -1,21 +1,26 @@
 
 resource "aws_instance" "instance" {
-     for_each = var.components
-      ami    = data.aws_ami.centos.image_id
-     instance_type = each.value["instance_type"]
-     vpc_security_group_ids =[data.aws_security_group.allow-all.id]
+  for_each               = var.components
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = each.value["instance_type"]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
 
   tags = {
     Name = each.value["name"]
   }
+}
+resource "null_resource" "provisioner" {
+  depends_on = [aws_instance.instance,aws_route53_record.records]
+  for_each = var.components
+
   provisioner "remote-exec" {
-  connection {
-    type     = "ssh"
-    user     = "centos"
-    password = "DevOps321"
-    host     = self.private_ip
-  }
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = [aws_instance.instance[each.value["name"]].private_ip]
+    }
     inline = [
       "rm -rf roboshop-shell",
       "git clone http://github.com/naveen2513/roboshop-shell.git",
@@ -23,7 +28,8 @@ resource "aws_instance" "instance" {
       "sudo bash ${each.value["name"]}.sh"
     ]
   }
-  }
+}
+
 resource "aws_route53_record" "records" {
   for_each = var.components
   zone_id = "Z09466133SH7C438NSMD2"
