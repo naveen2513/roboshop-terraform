3b7ccf36705138dace8c6e7468ee65b3b51b3e55
naveen p <naveenpendekatla0407@gmail.com>
2023-05-20 12:54:39 +0530

    servers

--- servers.tf
+++ servers.tf
@@ -16,6 +16,13 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 }
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z09466133SH7C438NSMD2"
+  name    = "frontend.naveendevops2.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
 resource "aws_instance" "mongodb" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
@@ -23,6 +30,13 @@ resource "aws_instance" "mongodb" {
   tags = {
     Name = "mongodb"
   }
+}
+resource "aws_route53_record" "monogodb" {
+  zone_id = "Z09466133SH7C438NSMD2"
+  name    = "frontend.naveendevops2.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
 }
 resource "aws_instance" "catalogue" {
   ami           = data.aws_ami.centos.image_id
@@ -32,6 +46,13 @@ resource "aws_instance" "catalogue" {
     Name = "catalogue"
   }
 }
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z09466133SH7C438NSMD2"
+  name    = "frontend.naveendevops2.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
 resource "aws_instance" "user" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
@@ -39,6 +60,13 @@ resource "aws_instance" "user" {
   tags = {
     Name = "user"
   }
+}
+resource "aws_route53_record" "user" {
+  zone_id = "Z09466133SH7C438NSMD2"
+  name    = "frontend.naveendevops2.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
 }
 resource "aws_instance" "cart" {
   ami           = data.aws_ami.centos.image_id
@@ -48,6 +76,13 @@ resource "aws_instance" "cart" {
     Name = "cart"
   }
 }
+resource "aws_route53_record" "cart" {
+  zone_id = "Z09466133SH7C438NSMD2"
+  name    = "frontend.naveendevops2.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
+}
 resource "aws_instance" "redis" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
@@ -55,6 +90,13 @@ resource "aws_instance" "redis" {
   tags = {
     Name = "redis"
   }
+}
+resource "aws_route53_record" "redis" {
+  zone_id = "Z09466133SH7C438NSMD2"
+  name    = "frontend.naveendevops2.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
 }
 resource "aws_instance" "mysql" {
   ami           = data.aws_ami.centos.image_id
@@ -64,6 +106,13 @@ resource "aws_instance" "mysql" {
     Name = "mysql"
   }
 }
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z09466133SH7C438NSMD2"
+  name    = "frontend.naveendevops2.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
 resource "aws_instance" "shipping" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
@@ -71,6 +120,13 @@ resource "aws_instance" "shipping" {
   tags = {
     Name = "shipping"
   }
+}
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z09466133SH7C438NSMD2"
+  name    = "frontend.naveendevops2.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
 }
 resource "aws_instance" "rabbitmq" {
   ami           = data.aws_ami.centos.image_id
@@ -80,6 +136,13 @@ resource "aws_instance" "rabbitmq" {
     Name = "rabbitmq"
   }
 }
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z09466133SH7C438NSMD2"
+  name    = "frontend.naveendevops2.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
 resource "aws_instance" "payment" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
@@ -87,4 +150,11 @@ resource "aws_instance" "payment" {
   tags = {
     Name = "payment"
   }
+}
+resource "aws_route53_record" "payment" {
+  zone_id = "Z09466133SH7C438NSMD2"
+  name    = "frontend.naveendevops2.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
 }
