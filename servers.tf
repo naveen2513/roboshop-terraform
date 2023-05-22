53f9e9f2a4d93a1f2842a672229611b8a9e99078
naveen p <naveenpendekatla0407@gmail.com>
2023-05-22 20:46:19 +0530

    servers

--- servers.tf
+++ servers.tf
@@ -1,3 +1,55 @@
+data "aws_ami" "centos" {
+  owners = "973714476881"
+}
+data "aws_security_group" "allow-all" {
+  name = "allow-all"
+}
+
+variable "components" {
+  default ={
+    frontend ={
+      name = "frontend"
+      instant_type = "t3.micro"
+    }
+    mongodb ={
+      name = "mongodb"
+      instant_type = "t3.micro"
+    }
+    catalogue ={
+      name = "catalogue"
+      instant_type = "t3.micro"
+    }
+    redis ={
+      name = "redis"
+      instant_type = "t3.micro"
+    }
+    cart ={
+      name = "cart"
+      instant_type = "t3.micro"
+    }
+    user ={
+      name = "user"
+      instant_type = "t3.micro"
+    }
+    mysql ={
+      name = "mysql"
+      instant_type = "t3.micro"
+    }
+    shipping ={
+      name = "shipping"
+      instant_type = "t3.micro"
+    }
+    rabbitmq ={
+      name = "rabbitmq"
+      instant_type = "t3.micro"
+    }
+    payment ={
+      name = "payment"
+      instant_type = "t3.micro"
+    }
+  }
+}
+
 
 resource "aws_instance" "instance" {
   for_each               = var.components
